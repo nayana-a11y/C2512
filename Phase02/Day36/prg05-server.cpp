6f26617216df145547b22256e22fae40a1b3d616
nayana-a11y <nayanahari613o@gmail.com>
2025-01-09 22:31:03 +0530

    Create prg05-server.cpp

---
+++ Phase02/Day36/prg05-server.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void run_server() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+    const char* response = "Hello from server";
+
+    // Create socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind socket to port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for connections
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Server is listening on port " << PORT << std::endl;
+
+    // Accept a connection
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("Accept failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Read data from client
+    read(new_socket, buffer, BUFFER_SIZE);
+    std::cout << "Received from client: " << buffer << std::endl;
+
+    // Send response to client
+    send(new_socket, response, strlen(response), 0);
+    std::cout << "Response sent to client" << std::endl;
+
+    // Close sockets
+    close(new_socket);
+    close(server_fd);
+}
+
+int main() {
+    run_server();
+    return 0;
+}
