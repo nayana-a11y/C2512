22c1373482ade20043f416bd697d4941b3999c5e
nayana-a11y <nayanahari613o@gmail.com>
2025-01-13 22:25:25 +0530

    Update Task.cpp

--- Phase02/Day37/Task.cpp
+++ Phase02/Day37/Task.cpp
@@ -1,143 +1,43 @@
 #include <iostream>
-#include <vector>
 #include <thread>
-#include <future>
-#include <algorithm>
-#include <cstring>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <unistd.h>
+#include <vector>
 
-// Function to calculate the sum of a vector
-void calculateSum(const std::vector<int>& vec, std::promise<int> sumPromise) {
+// two threads
+void findSum(std::vector<int> &vec);
+void findMin(std::vector<int> &vec);
+
+int main()
+{
+    std::vector<int> numbers = {3, 5, 2, 6, 1, 9, 10, 7};
+
+    std::thread threadSum(findSum, std::ref(numbers));
+    std::thread threadMin(findMin, std::ref(numbers));
+
+    threadSum.join();
+    threadMin.join();
+
+    return 0;
+}
+
+void findSum(std::vector<int> &vec)
+{
     int sum = 0;
-    for (int num : vec) {
-        sum += num;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        sum+=vec[i];
     }
-    sumPromise.set_value(sum); // Pass the result via promise
+    std::cout << "The sum of values of the vector is: " << sum << std::endl;
 }
 
-// Function to find the minimum element of a vector
-void findMin(const std::vector<int>& vec, std::promise<int> minPromise) {
-    int minVal = *std::min_element(vec.begin(), vec.end());
-    minPromise.set_value(minVal); // Pass the result via promise
-}
-
-// Function for server process
-void server(int port) {
-    const char* shmName = "/shared_memory";
-    const size_t shmSize = 1024;
-
-    // Create shared memory
-    int shmFd = shm_open(shmName, O_CREAT | O_RDWR, 0666);
-    if (shmFd == -1) {
-        perror("shm_open failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set the size of shared memory
-    if (ftruncate(shmFd, shmSize) == -1) {
-        perror("ftruncate failed");
-        close(shmFd);
-        shm_unlink(shmName);
-        exit(EXIT_FAILURE);
-    }
-
-    // Map shared memory
-    void* shmPtr = mmap(0, shmSize, PROT_WRITE, MAP_SHARED, shmFd, 0);
-    if (shmPtr == MAP_FAILED) {
-        perror("mmap failed");
-        close(shmFd);
-        shm_unlink(shmName);
-        exit(EXIT_FAILURE);
-    }
-
-    // Request numbers from the user
-    std::vector<int> numbers;
-    int n;
-    std::cout << "Enter the number of elements: ";
-    std::cin >> n;
-    std::cout << "Enter " << n << " numbers:\n";
-    for (int i = 0; i < n; ++i) {
-        int num;
-        std::cin >> num;
-        numbers.push_back(num);
-    }
-
-    // Promises and futures for sum and minimum
-    std::promise<int> sumPromise, minPromise;
-    std::future<int> sumFuture = sumPromise.get_future();
-    std::future<int> minFuture = minPromise.get_future();
-
-    // Create threads
-    std::thread sumThread(calculateSum, std::ref(numbers), std::move(sumPromise));
-    std::thread minThread(findMin, std::ref(numbers), std::move(minPromise));
-
-    // Join threads
-    sumThread.join();
-    minThread.join();
-
-    // Get the results
-    int sum = sumFuture.get();
-    int minVal = minFuture.get();
-
-    std::cout << "Server: Sum = " << sum << ", Min = " << minVal << std::endl;
-
-    // Write results to shared memory
-    std::string data = "Sum: " + std::to_string(sum) + ", Min: " + std::to_string(minVal);
-    std::copy(data.begin(), data.end(), static_cast<char*>(shmPtr));
-
-    // Unlink shared memory
-    shm_unlink(shmName);
-}
-
-// Function for client process
-void client() {
-    const char* shmName = "/shared_memory";
-
-    // Open shared memory
-    int shmFd = shm_open(shmName, O_RDONLY, 0666);
-    if (shmFd == -1) {
-        perror("shm_open failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Map shared memory
-    void* shmPtr = mmap(0, 1024, PROT_READ, MAP_SHARED, shmFd, 0);
-    if (shmPtr == MAP_FAILED) {
-        perror("mmap failed");
-        close(shmFd);
-        exit(EXIT_FAILURE);
-    }
-
-    // Read and print data from shared memory
-    std::cout << "Client received: " << static_cast<char*>(shmPtr) << std::endl;
-
-    // Close shared memory
-    close(shmFd);
-}
-
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage:\n"
-                  << "\t./program server <port>\n"
-                  << "\t./program client\n";
-        return EXIT_FAILURE;
-    }
-
-    if (strcmp(argv[1], "server") == 0) {
-        if (argc != 3) {
-            std::cerr << "Usage: ./program server <port>\n";
-            return EXIT_FAILURE;
+void findMin(std::vector<int> &vec)
+{
+    int min = vec[0];
+    for (int i = 1; i < vec.size(); i++)
+    {
+        if (vec[i] < min)
+        {
+            min = vec[i];
         }
-        int port = std::stoi(argv[2]);
-        server(port);
-    } else if (strcmp(argv[1], "client") == 0) {
-        client();
-    } else {
-        std::cerr << "Invalid mode. Use 'server' or 'client'.\n";
-        return EXIT_FAILURE;
     }
-
-    return EXIT_SUCCESS;
+    std::cout << "The minimum value from the values of the vector is: " << min << std::endl;
 }
