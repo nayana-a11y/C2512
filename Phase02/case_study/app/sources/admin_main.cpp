f9e4d8d67bf9b1fc33357c1c82045263f152c43e
nayana-a11y <nayanahari613o@gmail.com>
2025-01-20 00:44:44 +0530

    Update admin_main.cpp

--- Phase02/case_study/app/sources/admin_main.cpp
+++ Phase02/case_study/app/sources/admin_main.cpp
@@ -17,20 +17,20 @@ class DoctorController {
             int flags = 127) {
 
             if((flags & 1) != 0) {
-                name = uiCommon.in.Str("Doctor Name:");
+                name = uiCommon.in.Str("Doctor Name : ");
             }
 
             if((flags & 2) != 0) {
-                gender = uiCommon.in.Int("Gender (1-Male, 2-Female, 3-Other):");
+                gender = uiCommon.in.Int("Gender (1-Male, 2-Female, 3-Other): ");
             }
 
             if((flags & 4) != 0) {
-                department = uiCommon.in.Int("Department (1-Cardiology, 2-Neurology, 3-ENT, 4-General Physician):");
+                department = uiCommon.in.Int("Department (1-Cardiology, 2-Neurology, 3-ENT, 4-General Physician): ");
             }
- 
+
             if((flags & 8) != 0) {
                 do {
-                    phone = uiCommon.in.Str("Enter Phone:");
+                    phone = uiCommon.in.Str("Enter Phone : ");
                     if(!uiCommon.in.exist()) {
                         break;
                     }
@@ -40,7 +40,7 @@ class DoctorController {
 
             if((flags & 16) != 0) {
                 do {
-                    email = uiCommon.in.Str("Enter email:");
+                    email = uiCommon.in.Str("Enter email : ");
                     if(!uiCommon.in.exist()) {
                         break;
                     }
@@ -49,16 +49,16 @@ class DoctorController {
             }
 
             if((flags & 32) != 0) {
-                location = uiCommon.in.Str("Enter Location:");
+                location = uiCommon.in.Str("Enter Location : ");
             }
 
             if((flags & 64) != 0) {
-                address = uiCommon.in.Str("Enter Address:");
+                address = uiCommon.in.Str("Enter Address : ");
             }
         }
 
         void Create() {
-            uiCommon.TitleBar("Admin App > Doctor Management > Create Doctor");
+            uiCommon.TitleBar("Admin > Doctor Management > Create Doctor");
             int flags = 127;
             std::string name;
             int gender = 0;
@@ -67,12 +67,12 @@ class DoctorController {
             std::string email;
             std::string location;
             std::string address;
-            
+
             do {
                 Read(name, gender, department, phone, email, location, address, flags);
-                int proceedOption; 
+                int proceedOption;
 
-                std::stringstream soutput; 
+                std::stringstream soutput;
                 soutput << "[1] edit `name`" << std::endl;
                 soutput << "[2] edit `gender`" << std::endl;
                 soutput << "[3] edit `department`" << std::endl;
@@ -85,11 +85,11 @@ class DoctorController {
                 soutput << "[10] edit `gender` and `department`" << std::endl;
                 soutput << "[11] edit `Location` and `Address`" << std::endl;
                 soutput << "[91] Confirm To Create" << std::endl;
-                soutput << "Enter your choice:"; 
+                soutput << "Enter your choice : "; 
                 proceedOption = uiCommon.in.Int(soutput.str());
 
                 if(91 == proceedOption) {
-                    //set to biz object
+                    // Set to biz object
                     break;
                 }
                 flags = proceedOption;
@@ -101,33 +101,53 @@ class DoctorController {
         }
 
         void Edit() {
-            uiCommon.TitleBar("Admin App > Doctor Management > Edit Doctor");
+            uiCommon.TitleBar("Admin > Doctor Management > Edit Doctor");
             uiCommon.PressAnyKey(true); 
         }
 
         void Delete() {
-            uiCommon.TitleBar("Admin App > Doctor Management > Delete Doctor");
+            uiCommon.TitleBar("Admin > Doctor Management > Delete Doctor");
             uiCommon.PressAnyKey(true); 
         }
 
         void DisplayAll() {
-            uiCommon.TitleBar("Admin App > Doctor Management > List of Doctors");
+            uiCommon.TitleBar("Admin > Doctor Management > List of Doctors");
             uiCommon.PressAnyKey(true); 
+        }
+
+        // Added missing methods
+        void View() {
+            uiCommon.TitleBar("Admin > Doctor Management > View Doctor");
+            std::cout << "Viewing doctor details..." << std::endl;
+            uiCommon.PressAnyKey(true);
+        }
+
+        void Search() {
+            uiCommon.TitleBar("Admin > Doctor Management > Search Doctor");
+            std::cout << "Searching for doctor..." << std::endl;
+            uiCommon.PressAnyKey(true);
+        }
+
+        void Deactivate() {
+            uiCommon.TitleBar("Admin > Doctor Management > Deactivate Doctor");
+            std::cout << "Deactivating doctor..." << std::endl;
+            uiCommon.PressAnyKey(true);
         }
 };
 
 static int ReadDoctorMenu() {
     int choice;
     
-    uiCommon.TitleBar("Admin App > Doctor Management");
+    uiCommon.TitleBar("Admin > Doctor Management");
 
     std::stringstream soutput;
-    soutput << "1 - Create Doctor" << std::endl;
-    soutput << "2 - Edit Doctor" << std::endl;
-    soutput << "3 - Delete Doctor" << std::endl;
-    soutput << "4 - Display All Doctors" << std::endl;
-    soutput << "99 - Exit" << std::endl;
-    soutput << "Your choice:"; 
+    soutput << "[1] create doctor" << std::endl;
+    soutput << "[2] edit doctor" << std::endl;
+    soutput << "[3] view doctor" << std::endl;
+    soutput << "[4] search doctor" << std::endl;
+    soutput << "[5] deactivate doctor" << std::endl;
+    soutput << "[99] exit" << std::endl;
+    soutput << "Enter your choice : "; 
     choice = uiCommon.in.Int(soutput.str());
     
     uiCommon.Line('~');
@@ -153,11 +173,14 @@ void ManageDoctor() {
                 controller.Edit();
             } break;
             case 3: {
-                controller.Delete();
+                controller.View();
             } break;
             case 4: {
-                controller.DisplayAll();
+                controller.Search();
+            } break;
+            case 5: {
+                controller.Deactivate();
             } break;
         }
     } while(99 != choice);
-}
+} 
